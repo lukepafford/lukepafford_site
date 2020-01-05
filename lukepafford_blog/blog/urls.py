7028b7dc2dfbfacdfb63b117c93d752a3b66e32d
Luke Pafford <lukepafford@gmail.com>
2020-01-05 02:08:58 -0800

    Create barebones form for uploading blog posts

--- lukepafford_blog/blog/urls.py
+++ lukepafford_blog/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView, BlogList
+from .views import HomeView, BlogList, BlogUpload
 
 urlpatterns = [
     path("", BlogList.as_view(), name="blog_list"),
+    path("upload", BlogUpload.as_view(), name="blog_upload"),
 ]
--- lukepafford_blog/blog/views.py
+++ lukepafford_blog/blog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
 from django.views import View
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Posts
+from .forms import UploadPostForm
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -13,5 +14,13 @@ class HomeView(TemplateView):
 class BlogList(View):
     def get(self, request):
         posts = Posts.objects.all()
-
         return render(request, "blog/blog_list.html", {"posts": posts})
+
+
+class BlogUpload(View):
+    def get(self, request):
+        form = UploadPostForm
+        return render(request, "blog/blog_upload.html", {"form": form})
+
+    def post(self, request):
+        return redirect("blog_list")
