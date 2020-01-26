eaf03d67ab4cfbd07807efa512753b994d687ea3
Luke Pafford <lukepafford@gmail.com>
2020-01-26 01:03:09 -0800

    Update pipeline to automatically copy static assets to CDN

--- lukepafford_blog/lukepafford_blog/settings.py
+++ lukepafford_blog/lukepafford_blog/settings.py
@@ -182,7 +182,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = "/static/"
+if DEBUG:
+    STATIC_URL = "/static/"
+else:
+    STATIC_URL = "https://lukepafford.com/cdn/lukepafford_blog/"
+
+STATIC_ROOT = "static"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "public"),
+    os.path.join(BASE_DIR, "public/dist"),
 ]
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-lukepafford-blog",
-    version="1.0.19",
+    version="1.0.21",
     description="Django site for personal blog, and anything else",
     url="https://github.com/lukepafford/lukepafford_site",
     author="Luke Pafford",
@@ -17,6 +17,7 @@ setup(
     package_dir={"": "lukepafford_blog"},
     packages=find_packages("lukepafford_blog"),
     include_package_data=True,
+    entry_points={"console_scripts": ["manage.py = lukepafford_blog:manage",]},
     install_requires=[
         "django",
         "django-allauth",
